d08b385e46a507040d2a9552d6e4ba73a98ccfe5
sharkvn1 <richberchannel01@gmail.com>
2023-06-02 03:28:57 +0700

    + config primary color and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,11 +8,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sanpro: ['Source Sans Pro', 'Helvetica', 'Arial', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
       },
+      colors: {
+        'primary-color': '#cd1818',
+      }
     },
   },
   plugins: [],
