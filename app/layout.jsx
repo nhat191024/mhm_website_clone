6a4c1e050233db63ef3aeeae8b65df5c1a9da9f8
sharkvn1 <richberchannel01@gmail.com>
2023-06-08 05:16:07 +0700

    + change bg color and add darkmode

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,38 @@
+"use client"
+
 import '@styles/globals.css';
 import Nav from '@components/navBar'
+import { useEffect } from 'react';
 
 export const metadata = {
-    title: 'mhm clone',
-    description: 'mhm clone',
+  title: 'mhm clone',
+  description: 'mhm clone',
 }
 const rootLayout = ({ children }) => {
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour > 18 || currentHour < 6) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, []);
+
   return (
     <html lang='vi'>
-        <body>
-            <div className="main">
-                <div className="gradient" />
-            </div>
-            <main className="app">
-              <Nav />
-                {children}
-            </main>
-        </body>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>
+        <div className="">
+          <div className=' bg-secondary-color' />
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
