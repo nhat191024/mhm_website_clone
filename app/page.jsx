8f5dbb567e525611658f878031e86e5c3a4d0ecf
sharkvn1 <richberchannel01@gmail.com>
2023-06-08 05:16:26 +0700

    + add slider to home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,9 @@
+import Slider from '@components/slideShow'
+
 const Home = () => {
   return (
-    <section className="w-full flex-center flex-col">
+    <section className="w-full flex items-center justify-center flex-col">
+      <Slider />
       <h1>test</h1>
     </section>
   )
