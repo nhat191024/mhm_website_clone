4c55978ab1c1b40cbf14074d27bff190c22f29c2
sharkvn1 <richberchannel01@gmail.com>
2023-06-11 05:20:14 +0700

    + add some color

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,11 @@ module.exports = {
       colors: {
         'primary-color': '#cd1818',
         'secondary-color': '#c9c9c9',
+        'footer-color': '#222222',
+        'facebook-color': '#3B5998',
+        'instagram-color': '#C32AA3',
+        'youtube-color': '#FF0000',
+        'taktak-color': '#000000',
       }
     },
   },
