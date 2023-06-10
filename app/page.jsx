0baf26025fb08281137113e90f09c826e3aae291
sharkvn1 <richberchannel01@gmail.com>
2023-06-11 05:23:07 +0700

    + complete home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,16 @@
 import Slider from '@components/homepage/slideShow'
 import FlashSale from '@components/homepage/flashSale'
-import Footer from '@components/footer'
+import Product from '@components/homepage/product'
+import Info from '@components/homepage/info'
 
 const Home = () => {
   return (
     <section className="w-full flex items-center justify-center flex-col">
       <Slider />
       <FlashSale />
+      <Product />
+      <Info />
 
-      {/* <Footer /> */}
     </section>
   )
 }
