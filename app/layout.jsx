d3a5d51bbb89446e5f3f067b84e7a9acc357b272
sharkvn1 <richberchannel01@gmail.com>
2023-10-09 08:33:17 +0700

    + update google analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import '@styles/globals.css';
+import GoogleAnalytics from './GoogleAnalytics';
 import Nav from '@components/navBar'
 import Footer from '@components/footer'
 import Social from '@components/socialMedia'
@@ -15,21 +16,13 @@ const rootLayout = ({ children }) => {
     <html lang='vi'>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {/* Google tag (gtag.js */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KX3B52PJD1"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-KX3B52PJD1');
-        </script>
       </head>
       <body>
         <div className="">
           <div className=' bg-secondary-color' />
         </div>
         <main className="app">
+          <GoogleAnalytics />
           <Nav />
           <Social />
           {children}
